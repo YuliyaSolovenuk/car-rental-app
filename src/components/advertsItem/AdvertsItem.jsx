20ba3d46b595782113c9e532bc860e63a2c38067
Yulia Solovenuk <121509951+YuliyaSolovenuk@users.noreply.github.com>
2023-11-21 00:02:58 +0200

    Update AdvertsItem.jsx

--- src/components/advertsItem/AdvertsItem.jsx
+++ src/components/advertsItem/AdvertsItem.jsx
@@ -30,7 +30,7 @@ const AdvertsItem = ({ advert }) => {
   return (
     <Card>
       <CardMedia
-        sx={{ height: 268, width: 274, objectFit: 'contain' }}
+        sx={{ height: 268, objectFit: 'cover' }}
         image={img}
         alt={make}
       />
