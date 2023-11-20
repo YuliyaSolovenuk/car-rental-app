6e94a01fe8a909e613a6b0750a027108c3fa2cf2
YuliyaSolovenuk <121509951+YuliyaSolovenuk@users.noreply.github.com>
2023-11-20 21:41:54 +0000

    Deploying to gh-pages from @ YuliyaSolovenuk/car-rental-app@8a9b6ae6bb94ce3db64361bb1da26e430314be10 🚀

---
+++ static/js/683.6399463c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rental_app=self.webpackChunkcar_rental_app||[]).push([[683],{3683:(a,e,r)=>{r.r(e),r.d(e,{default:()=>s});var p=r(184);const s=()=>(0,p.jsx)("div",{style:{marginTop:"100px"},children:"Favorites"})}}]);
+//# sourceMappingURL=683.6399463c.chunk.js.map
